a9ce78cb0ec03452b04b1be0106615c5ef87d8e7
Camillo Rinaldi <camillo_rinaldi@yahoo.com.br>
2017-07-07 00:18:46 -0300

    Sorting e Salvar nas fitas.

--- Intercalacao Balanceada.cpp
+++ Intercalacao Balanceada.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstring>
+#include <algorithm>
 
 #define TAMAREA 100
 #define F 20
@@ -32,15 +33,22 @@ typedef TipoItem TipoRegistro;
 typedef struct Aluno {
     char inscricao[9];
     char nota[6];
+    double notaf;
     char estado[3];
-    char cidade[52];
-    char curso[31];
+    char cidade[51];
+    char curso[30];
 } Aluno;
 // Memoria para 19 registros
 
 int iniciaFitas();
 
 int preencheMemoriaPrincipal();
+
+int ordenarMemoriaPrincipal();
+
+void imprimeMemoriaPrincipal();
+
+int salvaNaFita();
 
 FILE **fitas = new FILE *[F];
 FILE *arq;
@@ -50,8 +58,9 @@ int main() {
     arq = fopen("PROVAO.TXT", "r");
     iniciaFitas();
     preencheMemoriaPrincipal();
+    ordenarMemoriaPrincipal();
+    salvaNaFita();
 
-    cout << "done" << endl;
     return 0;
 }
 
@@ -59,15 +68,41 @@ int iniciaFitas() {
     char nome[15];
     for (int i = 1; i < F + 1; i++) {
         sprintf(nome, "fita%d.txt", i);
-        fitas[i] = fopen(nome, "w+");
+        fitas[i] = fopen(nome, "w");
     }
 }
 
 int preencheMemoriaPrincipal() {
-    char eol;
-    for (int i = 0; i < 19; i++) {
-        fscanf(arq, "%s %s %s %51c %30c", memPrincipal[i].inscricao, memPrincipal[i].nota, memPrincipal[i].estado, memPrincipal[i].cidade, memPrincipal[i].curso);
-
+    for (int i = 0; i < F-1; i++) {
+        fscanf(arq, "%s %s %s %51c %30c", memPrincipal[i].inscricao, memPrincipal[i].nota, memPrincipal[i].estado,
+               memPrincipal[i].cidade, memPrincipal[i].curso);
+        memPrincipal[i].notaf = atof(memPrincipal[i].nota);
     }
     return 0;
+}
+
+bool compare(Aluno const &a, Aluno const &b) {
+    return a.notaf > b.notaf;
+}
+
+int ordenarMemoriaPrincipal() {
+    std::sort(memPrincipal,memPrincipal + 19,compare);
+    return 0;
+}
+
+void imprimeMemoriaPrincipal(){
+    for(int i = 0; i < F-1;i++){
+        cout << memPrincipal[i].notaf << " ";
+    }
+}
+
+int salvaNaFita(){
+    FILE *fita;
+    char nome[15];
+    for(int i = 0;i<F-1;i++) {
+        sprintf(nome, "fita%d.txt", i+1);
+        fita = fopen(nome, "a");
+        fprintf(fita,"%s %s %s %.50s %.30s\n", memPrincipal[i].inscricao, memPrincipal[i].nota, memPrincipal[i].estado,
+                memPrincipal[i].cidade, memPrincipal[i].curso);
+    }
 }
