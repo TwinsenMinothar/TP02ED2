e45ee94214fdc1d4b8df178ce477c54dce8a6cee
GabrielNathan7 <gabrielnata07@gmail.com>
2017-07-20 02:07:59 -0300

    Add files via upload

---
+++ VariosCaminhos.h
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "Aluno.h"
+#include <string>
+
+using namespace std;
+
+#ifndef FITA_H
+#define FITA_H
+
+typedef struct{
+   int fitas;
+   int contaFitas[10];
+   bool estado[10];
+}Fita;
+
+void criaBlocos(int quantidade, string nomeArq);
+void intercala(int quantidade);
+bool compare(Aluno &a, Aluno &b);
+#endif
